0093f232f50fe312fd39ca3f0758cbbfba2183b7
Aryan Prince <aryanprincesanty@icloud.com>
2023-01-07 00:54:31 +0300

    :rocket: Initial commit

---
+++ TipCalculatorWcfService/IService1.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace TipCalculatorWcfService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        double CalculateTip(double billAmount, int numPeople, double tipPercent);
+
+        [OperationContract]
+        double CalculateTotalWithTip(double billAmount, int numPeople, double tipPercent);
+
+        [OperationContract]
+        double CalculateTotalPerPerson(double billAmount, int numPeople, double tipPercent);
+    }
+}
---
+++ TipCalculatorWcfService/Service1.svc.cs
@@ -0,0 +1,23 @@
+﻿namespace TipCalculatorWcfService
+{
+    public class Service1 : IService1
+    {
+        public double CalculateTip(double billAmount, int numPeople, double tipPercent)
+        {
+            // Calculate the tip
+            return billAmount * (tipPercent / 100);
+        }
+
+        public double CalculateTotalWithTip(double billAmount, int numPeople, double tipPercent)
+        {
+            // Adds the tip to the bill amount
+            return billAmount + (billAmount * (tipPercent / 100));
+        }
+
+        public double CalculateTotalPerPerson(double billAmount, int numPeople, double tipPercent)
+        {
+            // Adds the tip to the bill amount and divides this total by the number of people
+            return (billAmount + (billAmount * tipPercent / 100)) / numPeople;
+        }
+    }
+}
